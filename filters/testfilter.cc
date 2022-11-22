da51085b87ec3cf1529985a99c0629a9eedd7a7f
Stefan Westerfeld <stefan@space.twc.de>
2022-11-22 17:57:50 +0100

    FILTERS: implement splitting from R into R1, R2 for HP4 / LP4 / BP4
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- filters/testfilter.cc
+++ filters/testfilter.cc
@@ -4,6 +4,7 @@
 
 #include <sys/time.h>
 #include <string>
+#include <complex>
 
 using std::string;
 using std::vector;
@@ -132,6 +133,60 @@ main (int argc, char **argv)
 
       filter.set_params (atoi (argv[4]), atof (argv[5]));
       filter.process_block (left.size(), left.data(), right.data(), freq.data());
+
+      vector<float> out;
+      for (size_t i = 0; i < left.size(); i++)
+        {
+          out.push_back (left[i]);
+          out.push_back (right[i]);
+        }
+
+      test (out);
+    }
+  if (argc == 6 && cmd == "4pole")
+    {
+      // roots of 4th order butterworth in s, left semi-plane
+      double sq2inv = 1 / sqrt (2);
+      std::complex<double> r1 {-sq2inv,  sq2inv};
+      std::complex<double> r2 {-sq2inv, -sq2inv};
+
+      // R must be in interval [0:1]
+      std::complex<double> R = 1 - atof (argv[5]);
+      std::complex<double> a1 = R + std::sqrt (R * R - 1.0);
+
+      // roots with resonance
+      std::complex<double> r1res = r1 * std::sqrt (a1);
+      std::complex<double> r2res = r2 * std::sqrt (a1);
+
+      auto R1 = -r1res.real();
+      auto R2 = -r2res.real();
+      printf ("# HH(s)=1/((s*s+2*s*%f+1)*(s*s+2*s*%f+1))\n", R1, R2);
+
+      vector<float> left;
+      vector<float> right;
+      vector<float> freq;
+      vector<float> samples = gen();
+
+      const float xfreq = atof (argv[2]);
+      for (size_t i = 0; i < samples.size() / 2; i++)
+        {
+          left.push_back (samples[i * 2]);
+          right.push_back (samples[i * 2 + 1]);
+          freq.push_back (xfreq);
+        }
+
+      SKFilter filter (atoi (argv[3]));
+      SKFilter filter2 (atoi (argv[3]));
+
+      /* test how the filter behaves as a linear filter (without distortion) */
+      float pre_scale = 0.001;
+      filter.set_scale (pre_scale, 1 / pre_scale);
+      filter2.set_scale (pre_scale, 1 / pre_scale);
+
+      filter.set_params (atoi (argv[4]), 1 - R1);
+      filter2.set_params (atoi (argv[4]), 1 - R2);
+      filter.process_block (left.size(), left.data(), right.data(), freq.data());
+      filter2.process_block (left.size(), left.data(), right.data(), freq.data());
 
       vector<float> out;
       for (size_t i = 0; i < left.size(); i++)
