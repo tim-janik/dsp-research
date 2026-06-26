4c29e0a77d292582e3d9e4ff66c01ddd7531028f
Stefan Westerfeld <stefan@space.twc.de>
2026-06-26 14:13:53 +0200

    distortion: add basic ADAA table for approximating f(x) and F(x)
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ distortion/adaatable.hh
@@ -0,0 +1,59 @@
+#include <functional>
+
+template<int RANGE, int BINS>
+class ADAATable
+{
+  std::array<float, BINS + 1> f_;
+  std::array<float, BINS + 1> F_;
+
+public:
+  ADAATable (std::function<double(double)> f)
+  {
+    // build function f(x) table
+    for (size_t i = 0; i < BINS + 1; i++)
+      f_[i] = f ((i / double (BINS) * 2 - 1) * RANGE);
+
+    // build antiderivative F(x) table
+    double Fx = 0;
+    double dx = 2.0 * RANGE / BINS;
+    F_[0] = Fx;
+
+    for (size_t i = 0; i < BINS; i++)
+      {
+        Fx += 0.5f * dx * (f_[i] + f_[i+1]);
+        F_[i + 1] = Fx;
+      }
+  }
+
+  float
+  f (float x)
+  {
+    if (x <= -RANGE)
+      return f_[0];
+    if (x >= RANGE)
+      return f_[BINS];
+
+    float fbin = (x + float (RANGE)) * float (BINS / (RANGE * 2));
+    int ibin = (int) fbin;
+
+    float frac = fbin - ibin;
+    return f_[ibin] + frac * (f_[ibin + 1] - f_[ibin]);
+  }
+
+  float
+  F (float x)
+  {
+    if (x <= -RANGE)
+      return F_[0] + (x + RANGE) * f_[0];
+    if (x >= RANGE)
+      return F_[BINS] + (x - RANGE) * f_[BINS];
+
+    float fbin = (x + float (RANGE)) * float (BINS / (RANGE * 2));
+    int ibin = (int) fbin;
+
+    float frac = fbin - ibin;
+    float dx = frac * 2.f * RANGE / BINS;
+    return F_[ibin] + dx * f_[ibin] + 0.5f * dx * dx * (f_[ibin+1] - f_[ibin]) / (2.f * RANGE / BINS);;
+  }
+
+};
