2a1324d9eef19646cca96f2888abb388765921c3
Stefan Westerfeld <stefan@space.twc.de>
2026-09-05 18:41:03 +0200

    distortion: mki.sh: speed up build

--- distortion/mki.sh
+++ distortion/mki.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 set -e
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithAssert
-make
+
+if [ ! -f CMakeCache.txt ]; then
+    cmake .. -DCMAKE_BUILD_TYPE=RelWithAssert
+fi
+
+make -j"$(nproc)"
 cp -avr DistortionPlugin_artefacts/RelWithAssert/VST3/DistortionPlugin.vst3 ~/.vst3
